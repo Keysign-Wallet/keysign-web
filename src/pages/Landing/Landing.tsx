49dcb92d8d331d7a7f1f5aafa2616505a5fb833f
Akin Aguda <lordtitan7@gmail.com>
2021-01-15 22:39:25 +0100

    added dynamic classes

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Landing.scss';
 
-const Landing: React.FC = () => <div className="Landing">Landing Component</div>;
+const Landing: React.FC = () => (
+  <div className="Landing background-keysign-light text-keysign-primary">Landing Component</div>
+);
 
 export default Landing;
