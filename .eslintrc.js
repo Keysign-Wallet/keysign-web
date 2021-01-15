0f9cff6521570fb29a7a707ad73b6b783e096bc9
mrbusysky <mrskymn@icloud.com>
2021-01-15 02:53:23 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,59 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
     'airbnb-typescript',
     'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    'prettier/react',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
-    project: 'tsconfig.json',
+    ecmaVersion: 2020,
+    project: './tsconfig.json',
     sourceType: 'module',
   },
-  env: {
-    node: true,
-    browser: true,
-    jest: true,
-  },
-  plugins: ['@typescript-eslint', 'eslint-plugin-import-helpers'],
+  plugins: ['react', '@typescript-eslint', 'jest'],
   rules: {
-    'import-helpers/order-imports': [
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'consistent-return': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'no-param-reassign': 'off',
+    'prettier/prettier': [
       'error',
       {
-        groups: ['/^react/', 'module', ['parent', 'sibling', 'index'], '/.*(css|scss)$/'],
-        newlinesBetween: 'always',
+        endOfLine: 'auto',
       },
     ],
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        max: 1,
-      },
-    ],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
+    'react/button-has-type': 'off',
+    'react/jsx-curly-brace-presence': 'off',
+    'react/no-array-index-key': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'sort-keys': 'error',
   },
 };
