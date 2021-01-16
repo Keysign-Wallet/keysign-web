a1f8ddc21123759e9cc6b2cfbe5c326b0653f71c
mrbusysky <mrskymn@icloud.com>
2021-01-15 23:54:01 -0800

    Update TopNavLoggedOut.tsx

--- src/containers/TopNav/TopNavLoggedOut/TopNavLoggedOut.tsx
+++ src/containers/TopNav/TopNavLoggedOut/TopNavLoggedOut.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
 import './TopNavLoggedOut.scss';
+import { NavLink } from 'react-router-dom';
 
-const TopNavLoggedOut: React.FC = () => <div className="TopNavLoggedOut">TopNavLoggedOut Component</div>;
+const TopNavLoggedOut: React.FC = () => {
+  return (
+    <div className="navBar">
+      <NavLink className="navBar__Logo" to="/about">
+        <img alt="Keysign Logo" className="navBar__Logo__img" src={'/'} />
+      </NavLink>
+      <h1>Keysign</h1>
+      <ul className="navBar__items-list">
+        <li>
+          <NavLink to="/faq" activeClassName="selected">
+            FAQ
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/integration" activeClassName="selected">
+            Integration
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" activeClassName="selected">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+      <NavLink to="/login" activeClassName="selected">
+        <button>Login</button>
+      </NavLink>
+      <NavLink to="/downloads" activeClassName="selected">
+        <button>Download</button>
+      </NavLink>
+    </div>
+  );
+};
 
 export default TopNavLoggedOut;
