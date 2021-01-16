6fc0639276cf12616a5b6859b9afa42b452cfaa1
AbhayAysola <abhay.ayyasola@gmail.com>
2021-01-16 11:51:21 +0530

    remove Footer in Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import './Layout.scss';
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div className="Layout">
       <div className="navBar">
+        <NavLink className="navBar__Logo" to="/about">
+          <img alt="Keysign Logo" className="navBar__Logo__img" src={'/'} />
+        </NavLink>
         <h1>Keysign</h1>
         <ul className="navBar__items-list">
           <li>FAQ</li>
@@ -15,9 +20,6 @@ const Layout: React.FC = ({ children }) => {
         <button>Download</button>
       </div>
       {children}
-      <div className="footer">
-        <h1>Footer</h1>
-      </div>
     </div>
   );
 };
