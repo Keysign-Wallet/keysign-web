69b2adc793753de29b32fcd6f2fafc7c358668d1
mrbusysky <mrskymn@icloud.com>
2021-01-16 18:34:21 -0800

    Delete handshake.ts

--- src/utils/handshake.ts
+++
@@ -1,4 +0,0 @@
-export const checkKeysign = (successCallback: () => void, failureCallback: () => void) => {
-  if (tnb_keysign) tnb_keysign.requestHandshake(successCallback);
-  else failureCallback();
-};
