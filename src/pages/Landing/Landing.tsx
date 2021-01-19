00cd06f5d47e3fd501656946fb8ef431c364e476
mrbusysky <mrskymn@icloud.com>
2021-01-19 14:27:29 -0800

    updating

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,21 +1,8 @@
-/* eslint-disable no-console */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Landing.scss';
 
-import { getKeysign } from '../../utils/handshake';
-
-const Landing: React.FC = () => {
-  const [keysign, setKeysign] = useState({});
-  useEffect(() => {
-    window.addEventListener('load', () => {
-      getKeysign(
-        (res: any) => setKeysign(res),
-        (err: any) => console.log(err)
-      );
-    });
-    console.log(keysign);
-  }, [keysign]);
-  return <div className="Landing background-keysign-light text-keysign-primary">Landing Component</div>;
-};
+const Landing: React.FC = () => (
+  <div className="Landing background-keysign-light text-keysign-primary">Landing Component</div>
+);
 
 export default Landing;
--- src/utils/handshake.js
+++
@@ -1,7 +0,0 @@
-export const getKeysign = (successCallback, failureCallback) => {
-  try {
-    window.tnb_keysign.requestHandshake(() => successCallback(window.tnb_keysign));
-  } catch (e) {
-    failureCallback(e);
-  }
-};
