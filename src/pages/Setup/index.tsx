7eb24d06bb1300baac3e1b63522c8ece597d377d
mrbusysky <mrskymn@icloud.com>
2021-01-19 16:43:13 -0800

    Update index.tsx

--- src/pages/Setup/index.tsx
+++ src/pages/Setup/index.tsx
@@ -4,5 +4,5 @@ import Setup from './Setup';
 const SetupContainer: React.FC = () => {
   const [show2fa, setShow2fa] = useState(false);
   return <Setup onToggle={() => setShow2fa(!show2fa)} show2fa={show2fa} />;
-
+};
 export default SetupContainer;
