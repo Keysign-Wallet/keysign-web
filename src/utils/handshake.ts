03a1e8d23c4c8ddc93e5fcd8c98180c515b0f9aa
mrbusysky <mrskymn@icloud.com>
2021-01-20 21:19:42 -0800

    update

--- src/utils/handshake.ts
+++ src/utils/handshake.ts
@@ -1,9 +1,10 @@
 import LoggerService from '../services/loggerService';
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
 declare const tnb_keysign: undefined | { requestHandshake: (successCallback: () => void) => void };
 
 export const checkKeysign = (successCallback: () => void, failureCallback: () => void) => {
-  LoggerService.log('Loggin tnb_keysign');
+  LoggerService.log('Login tnb_keysign');
   LoggerService.log(tnb_keysign);
   if (tnb_keysign) tnb_keysign.requestHandshake(successCallback);
   else failureCallback();
