ce478381388b3d3c050113c8ceeee2f987ba172c
mrbusysky <mrskymn@icloud.com>
2021-01-21 01:24:35 -0800

    Update KsLoginForm.tsx

--- src/components/KsLoginForm/KsLoginForm.tsx
+++ src/components/KsLoginForm/KsLoginForm.tsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import { ReactComponent as LoginSvg } from '../../assets/svgs/login.svg';
 
 import './KsLoginForm.scss';
 import ExtensionHelperService from '../../services/extensionHelperService';
 import BrowserStorageService from '../../services/browserStorageService';
+import LoggerService from '../../services/loggerService';
+
+const LOGIN_QUERY = gql`
+  mutation login($accountNumber: String!) {
+    login(accountNumber: $accountNumber) {
+      token
+      newUser
+    }
+  }
+`;
 
 const KsLoginForm: React.FC = () => {
   const [accountNumber, setAccountNumber] = useState('');
+  const [login, { data }] = useMutation(LOGIN_QUERY);
+
   const validateLogin = () => {
-    ExtensionHelperService.validateKSLogin(accountNumber).then((res: any) => {
-      BrowserStorageService.setItem('token', res.data.login.token);
-      BrowserStorageService.setItem('keysign', true);
-      BrowserStorageService.setItem('accountNumber', accountNumber);
+    ExtensionHelperService.validateKSLogin(accountNumber).then(() => {
+      login({ variables: { accountNumber } }).then((res: any) => {
+        LoggerService.log(data);
+        BrowserStorageService.setItem('token', res.data.login.token);
+        BrowserStorageService.setItem('keysign', true);
+        BrowserStorageService.setItem('accountNumber', accountNumber);
+      });
     });
   };
 
