7b4a1a9a854612003a6b8c7373851c7dbffdc61d
mrbusysky <mrskymn@icloud.com>
2021-01-26 21:29:00 -0800

    Revert "update"
    This reverts commit 8e3aff4155ddc675d6414c9727b97c86ccf19d62.

--- src/services/extensionHelperService.ts
+++ src/services/extensionHelperService.ts
@@ -1,4 +1,15 @@
+import { checkKeysign } from '../utils/handshake';
+
 export default class ExtensionHelperService {
+  static checkIfExtensionExists(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      checkKeysign(
+        () => resolve(true),
+        () => reject(new Error('Handshake failed'))
+      );
+    });
+  }
+
   static validateKSLogin(accountNumber: string): Promise<boolean> {
     return new Promise((resolve) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
