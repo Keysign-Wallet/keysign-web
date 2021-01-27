8e3aff4155ddc675d6414c9727b97c86ccf19d62
mrbusysky <mrskymn@icloud.com>
2021-01-26 21:23:47 -0800

    update

--- src/services/extensionHelperService.ts
+++ src/services/extensionHelperService.ts
@@ -1,15 +1,4 @@
-import { checkKeysign } from '../utils/handshake';
-
 export default class ExtensionHelperService {
-  static checkIfExtensionExists(): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      checkKeysign(
-        () => resolve(true),
-        () => reject(new Error('Handshake failed'))
-      );
-    });
-  }
-
   static validateKSLogin(accountNumber: string): Promise<boolean> {
     return new Promise((resolve) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
