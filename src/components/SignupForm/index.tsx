4cb9652f46cd7fcb245eda70067135307cb0e874
AbhayAysola <abhay.ayyasola@gmail.com>
2021-02-10 21:11:47 +0530

    Add SignupGenerateButton component

--- src/components/SignupForm/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import SignupForm from './SignupForm';
-
-const SignupFormContainer: React.FC = () => {
-  return <SignupForm />;
-};
-
-export default SignupFormContainer;
---
+++ src/components/SignupGenerateButton/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import SignupGenerateButton from './SignupGenerateButton';
+
+const SignupGenerateButtonContainer: React.FC = () => {
+  return <SignupGenerateButton />;
+};
+
+export default SignupGenerateButtonContainer;
