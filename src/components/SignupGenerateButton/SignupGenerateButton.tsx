48e037ba4b0ef8b7ca177945af58667b9865307f
AbhayAysola <abhay.ayyasola@gmail.com>
2021-02-11 18:44:07 +0530

    Add logic to generate accounts

--- src/components/SignupGenerateButton/SignupGenerateButton.tsx
+++ src/components/SignupGenerateButton/SignupGenerateButton.tsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 
 import './SignupGenerateButton.scss';
+import { createAccount } from '../../utils/tnb';
 import Expandable from '../common/Expandable';
 
 const SignupGenerateButton = () => {
   const [showGenerate, setShowGenerate] = useState(false);
+  const account = createAccount();
   return (
     <div className="SignupGenerateButton">
-      <button className="button SignupGenerateButton_button" onClick={() => setShowGenerate(!showGenerate)}>
-        Generate Wallet
-      </button>
-      <Expandable open={showGenerate}>GenerateWalletForm</Expandable>
+      {!showGenerate && (
+        <button className="button SignupGenerateButton_button" onClick={() => setShowGenerate(!showGenerate)}>
+          Generate Wallet
+        </button>
+      )}
+      <Expandable open={showGenerate} className="SignupGenerateButton_Expandable">
+        <form className="LoginForm__form">
+          <input type="text" className="input" value={account.accountNumberHex} />
+          <input type="text" className="input" value={account.signingKeyHex} />
+        </form>
+        <p className="paragraph text-keysign-grey">
+          Please copy your keys somewhere safe, if you lose these keys they cannot be recovered.
+        </p>
+        <button className="button SignupGenerateButton_Expandable_DownloadButton">Download Keys</button>
+        <button
+          onClick={() => setShowGenerate(!showGenerate)}
+          className="button SignupGenerateButton_Expandable_DoneButton text-keysign-primary background-keysign-light"
+        >
+          Done
+        </button>
+      </Expandable>
     </div>
   );
 };
--- src/utils/tnb.ts
+++ src/utils/tnb.ts
@@ -3,3 +3,7 @@ declare const tnb: any;
 export const checkValidSignInPair = (signingKey: string, accountNumber: string): boolean => {
   return tnb.Account.isValidPair(signingKey, accountNumber);
 };
+
+export const createAccount = (accountNumber?: string, signingKey?: string) => {
+  return new tnb.Account(signingKey, accountNumber);
+};
