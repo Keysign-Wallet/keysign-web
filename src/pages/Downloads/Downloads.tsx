9a094713e49e9e3b8c81500cf0a21dac920129f4
Fortune Ekeruo <legalmody@gmail.com>
2021-02-11 01:12:51 +0100

    add jsx markup for desktop downloads sections with svgs and imgs

--- src/pages/Downloads/Downloads.tsx
+++ src/pages/Downloads/Downloads.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import logo from '../../assets/images/logo-LARGE.png';
+import laptop from '../../assets/images/laptop.png';
 
 import { ReactComponent as Phone } from '../../assets/svgs/phone1.svg';
 import { ReactComponent as AppStoreBtn } from '../../assets/svgs/app-store.svg';
 import { ReactComponent as PlayStoreBtn } from '../../assets/svgs/play-store.svg';
+import { ReactComponent as WindowsLogo } from '../../assets/svgs/windows-logo.svg';
+import { ReactComponent as AppleLogo } from '../../assets/svgs/apple-logo.svg';
 
 import './Downloads.scss';
 
@@ -36,6 +39,26 @@ const Downloads: React.FC = () => {
           <button className="button">Get Started</button>
         </div>
         <img alt="Keysign Logo" className="img" src={logo} />
+      </section>
+
+      <section className="Downloads__section Downloads__desktop">
+        <div className="Downloads__desktop-1">
+          <div className="Downloads__desktop-windows">
+            <WindowsLogo />
+            <div className="Downloads__desktop-text">
+              <h2 className="text-keysign-dark-grey">Download Desktop version</h2>
+              <span className="text-keysign-grey">click here...</span>
+            </div>
+          </div>
+          <div className="Downloads__desktop-apple">
+            <AppleLogo />
+            <div className="Downloads__desktop-text">
+              <h2 className="text-keysign-dark-grey">Download Mac version</h2>
+              <span className="text-keysign-grey">click here...</span>
+            </div>
+          </div>
+        </div>
+        <img alt="Laptop computer" className="img" src={laptop} />
       </section>
     </div>
   );
