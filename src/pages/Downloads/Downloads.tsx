a01613a179b2b1660a0aa051831f06d1652d1182
Fortune Ekeruo <legalmody@gmail.com>
2021-02-11 01:00:31 +0100

    add mobile downloadssection with svg icons and image

--- src/pages/Downloads/Downloads.tsx
+++ src/pages/Downloads/Downloads.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
+
+import { ReactComponent as Phone } from '../../assets/svgs/phone1.svg';
+import { ReactComponent as AppStoreBtn } from '../../assets/svgs/app-store.svg';
+import { ReactComponent as PlayStoreBtn } from '../../assets/svgs/play-store.svg';
+
 import './Downloads.scss';
 
-const Downloads: React.FC = () => <div className="Downloads">Downloads Component</div>;
+const Downloads: React.FC = () => {
+  return (
+    <div className="Downloads">
+      <section className="Downloads__section Downloads__mobile">
+        <div>
+          <h1 className="title text-keysign-dark-grey">Download the Keysign Mobile App for a whole new experience.</h1>
+          <div className="Downloads__mobile-icons">
+            <a href="/downloads">
+              <AppStoreBtn />
+            </a>
+            <a href="/downloads">
+              <PlayStoreBtn />
+            </a>
+          </div>
+        </div>
+        <Phone />
+      </section>
+    </div>
+  );
+};
 
 export default Downloads;
