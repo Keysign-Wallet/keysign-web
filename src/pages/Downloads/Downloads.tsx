5be01b7b1eab9ef16f77cefbf3d89e429315818a
Fortune Ekeruo <legalmody@gmail.com>
2021-02-11 01:05:45 +0100

    add jsx markup for extension downloads sections with logo

--- src/pages/Downloads/Downloads.tsx
+++ src/pages/Downloads/Downloads.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import logo from '../../assets/images/logo-LARGE.png';
+
 import { ReactComponent as Phone } from '../../assets/svgs/phone1.svg';
 import { ReactComponent as AppStoreBtn } from '../../assets/svgs/app-store.svg';
 import { ReactComponent as PlayStoreBtn } from '../../assets/svgs/play-store.svg';
@@ -22,6 +24,18 @@ const Downloads: React.FC = () => {
           </div>
         </div>
         <Phone />
+      </section>
+
+      <section className="Downloads__section Downloads__extension">
+        <div>
+          <h1 className="title text-keysign-dark-grey">Browser Extension For Transaction Signing</h1>
+          <span className="subtitle text-keysign-grey">
+            Keysign aims to bring security and ease-of-use of Metamask to the blockchain platform, through the use of
+            secure keysign browser extension, available on Chrome and Firefox.
+          </span>
+          <button className="button">Get Started</button>
+        </div>
+        <img alt="Keysign Logo" className="img" src={logo} />
       </section>
     </div>
   );
