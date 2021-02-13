65eced12bf00aaf328c11302d8ffaf492cd6db26
Logan <lwendt220@gmail.com>
2021-02-13 14:05:39 -0600

    add "About Us" page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import './About.scss';
 
-const About: React.FC = () => <div className="About">About Component</div>;
+const About: React.FC = () => {
+  return (
+    <div className="About">
+      <h1 className="page-title">About Us</h1>
+      <p className="paragraph text-keysign-grey">Purus adipiscing tristique dis iaculis. Mi egestas posuere sit maecenas bibendum. Nulla a id neque, ullamcorper in netus sit. Lorem morbi egestas vel eget viverra in ut. Eleifend gravida eu, cras elementum consectetur ullamcorper dictum. Pharetra quisque in at volutpat quis senectus integer. Mi feugiat sit in cursus. Pellentesque eu arcu varius vitae, felis rutrum nunc id. Amet tincidunt etiam nec nec placerat orci neque, senectus. Fringilla sapien neque dictum suspendisse euismod. Purus varius cras tristique enim et vulputate morbi neque vestibulum. Est non lobortis porttitor aliquam facilisi libero. Sed ut mattis vel scelerisque. In purus tortor placerat non augue adipiscing. Sed nibh id et sit in nunc risus tristique aliquet.
+Vitae, convallis mi erat sem. Aliquam lorem mi elementum ullamcorper. Vivamus neque fringilla vitae lorem at facilisis diam. Id est luctus facilisis tincidunt. Tortor suspendisse odio nisl mi lectus.
+In ut ultrices lectus sed vitae id nisl sed. Duis blandit vulputate enim egestas rutrum. Id cras in sed ligula enim mauris lacus ipsum. Erat pellentesque imperdiet eget interdum urna. Orci integer vitae euismod suspendisse id tincidunt elementum odio. Ultrices ac id et malesuada purus. Amet sit porttitor nisl sodales platea augue nulla orci. Aliquam nullam neque, libero, accumsan cras vulputate quis dolor in. Leo justo turpis massa enim, suspendisse nulla. Etiam neque, pellentesque quisque praesent.
+      </p>
+      <p className="paragraph text-keysign-grey">
+Mauris est tellus lectus vulputate a. Cras magna diam at enim curabitur nulla bibendum aliquet. At lectus non morbi lacus at feugiat malesuada. Tellus proin amet, morbi velit vitae risus massa turpis tempor. Turpis ultrices leo aliquet pretium ac purus scelerisque odio.
+Elementum aliquam nulla scelerisque non in. Senectus sapien viverra tellus sed vitae ipsum. Faucibus et, turpis gravida id ullamcorper tempor fringilla et risus. Aliquam adipiscing mattis nunc et vestibulum, scelerisque sodales sit proin. Diam lectus eget id cursus id neque tristique tincidunt. Nunc et vitae lectus erat consequat mauris amet eget.
+Nullam sit gravida pretium habitasse justo, amet vitae vestibulum. Habitasse auctor velit etiam scelerisque in donec orci dolor sed. Venenatis sodales nisi sed amet ipsum. Sit egestas nibh dignissim diam vel. Praesent pharetra venenatis dapibus mus quam. Tellus tortor odio aliquam ante nunc sit netus accumsan. Vitae mauris pretium tincidunt semper. Penatibus etiam magna at fermentum, at scelerisque sit nibh placerat. Nibh magna aliquam eget aliquam. Tristique dui augue fringilla platea velit enim. Id adipiscing turpis eleifend rhoncus odio nisl lacus. Ipsum est viverra sollicitudin nulla faucibus hendrerit porttitor at. Amet felis, ut vel hendrerit massa luctus.
+Cras libero nulla vestibulum nulla sit velit tellus aliquam viverra. Morbi nibh sit in volutpat phasellus pharetra, porttitor. Varius cras condimentum viverra fusce volutpat quisque ultrices neque ut. Adipiscing nullam augue ipsum facilisis varius pellentesque. Eget at pellentesque aliquet nam tristique. Faucibus platea non, dictum dictum risus nunc malesuada. Maecenas turpis at volutpat quam eget.
+Massa massa bibendum quis sit tincidunt. Vulputate ut a eleifend id porta felis nunc aliquam id. Imperdiet risus sed suspendisse ipsum, vitae.
+      </p>
+    </div>
+  )
+}
 
 export default About;
