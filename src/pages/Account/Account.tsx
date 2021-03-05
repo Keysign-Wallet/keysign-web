c29b352b7cf99ac13d09bd3a8b17ccfa7a824108
Logan Wendt <lwendt220@gmail.com>
2021-03-04 21:33:55 -0600

    add skeleton for subpage selection

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Account.scss';
 
-const Account: React.FC = () => <div className="Account">Account Component</div>;
+enum Page {
+  SETTINGS,
+  SECURITY,
+  DELETE_ACCOUNT,
+}
+
+const renderSelectedPage = (page: Page) => {
+  switch (page) {
+    case Page.SETTINGS:
+      return <p>settings</p>;
+    case Page.SECURITY:
+      return <p>security</p>;
+    case Page.DELETE_ACCOUNT:
+      return <p>delete account</p>;
+    default:
+      return <p>invalid page selected</p>;
+  }
+};
+
+const Account: React.FC = () => {
+  const [selectedPage, setSelectedPage] = useState(Page.SETTINGS);
+
+  return (
+    <div className="Account">
+      <div className="Account__column">
+        <h1>Account</h1>
+        <p className="paragraph">You can make changes to your account here.</p>
+        <ul>
+          <li onClick={() => setSelectedPage(Page.SETTINGS)}>Settings</li>
+          <li onClick={() => setSelectedPage(Page.SECURITY)}>Security</li>
+          <li onClick={() => setSelectedPage(Page.DELETE_ACCOUNT)}>Delete Account</li>
+        </ul>
+      </div>
+      <div className="Account__content">{renderSelectedPage(selectedPage)}</div>
+    </div>
+  );
+};
 
 export default Account;
